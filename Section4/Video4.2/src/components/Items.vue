9e1ef5b03e76e4176851862b391dc2e7b4fca9ce
System Administrator <root@Anss-MacBook-Pro.local>
2019-02-24 13:24:39 +0500

    Section 4 video 4.2

---
+++ Section4/Video4.2/src/components/Items.vue
@@ -0,0 +1,210 @@
+<style>
+.ivu-table .demo-table-info-row td{
+    background-color: #2db7f5;
+    color: #fff;
+}
+.ivu-table .demo-table-error-row td{
+    background-color: #ff6600;
+    color: #fff;
+}
+.ivu-table td.demo-table-info-column{
+    background-color: #2db7f5;
+    color: #fff;
+}
+.ivu-table .demo-table-info-cell-name {
+    background-color: #2db7f5;
+    color: #fff;
+}
+.ivu-table .demo-table-info-cell-age {
+    background-color: #ff6600;
+    color: #fff;
+}
+.ivu-table .demo-table-info-cell-address {
+    background-color: #187;
+    color: #fff;
+}
+</style>
+
+<template>
+    <Layout>
+        <Header>
+
+          <div class="logo left">
+            <h3>LOGO</h3>
+          </div>
+          <div class="header-controls right">
+            <router-link to="signup" exact>
+                <i-button type="primary">Login</i-button>
+            </router-link>
+            <router-link to="signup" exact>
+                <i-button type="primary">Signup</i-button>
+            </router-link>
+          </div>
+        </Header>
+        <Content>
+            
+          <br >
+          <Row type="flex" justify="center">
+              <Col span="16">
+                <Card style="width:800px;">
+                <div>
+                    <p>Custom row styles:</p>
+                    <Table ref="table" size="large" :loading="loading" height="200" :row-class-name="rowClassName" stirped border :columns="columns1" :data="data1"></Table>    
+                    <Button type="primary" size="large" @click="exportData()">
+                        <Icon type="ios-download-outline"></Icon>
+                        Export source data
+                    </Button>
+                </div>
+                </Card>
+              </Col>
+          </Row>
+        </Content>
+        <Footer>
+          
+          <a href="#">Privacy & Policy</a> |
+          <a href="#">Contact Us</a> |
+          <a href="#">Terms & Conditions</a>
+
+        </Footer>
+    </Layout>
+</template>
+<script>
+export default {
+    name: 'Items',
+    data () {
+            return {
+                loading: false,
+                columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age',
+                        sortable: true
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                columns9: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age',
+                        className: 'demo-table-info-column'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ],
+                data8: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 25,
+                        address: 'London No. 1 Lake Park',
+                        cellClassName: {
+                            age: 'demo-table-info-cell-age',
+                            address: 'demo-table-info-cell-address'
+                        }
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        cellClassName: {
+                            name: 'demo-table-info-cell-name'
+                        }
+                    }
+                ]
+            }
+        },
+        methods: {
+            rowClassName (row, index) {
+                if (index === 1) {
+                    return 'demo-table-info-row';
+                } else if (index === 3) {
+                    return 'demo-table-error-row';
+                }
+                return '';
+            },
+            exportData(){
+                this.$refs.table.exportCsv({
+                    filename: 'the original data'
+                })
+            }
+        }
+}
+</script>
